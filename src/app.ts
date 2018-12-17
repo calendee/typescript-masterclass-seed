bc5977952192b62780fc1f89dc6939b79d07e98b
Justin Noel <justin.noel@calendee.com>
2018-12-17 14:43:19 -0600

    arrow functions and lexical scope

--- src/app.ts
+++ src/app.ts
@@ -44,65 +44,118 @@
 
 
 
+// /**
+//  * Exploring "this" with .call, .apply, and .bind
+//  */
+
+// // Object Literal
+// const myObj = {
+//   stuff: '123',
+//   myMethod() {
+//     console.log('Object : ', this);
+//   }
+// }
+
+// // Will console the object
+// // myObj.myMethod();
+
+
+// // Function
+// function myFunction(text1: string, text2) {
+//   console.log('Function : ', this, text1, text2);
+// }
+
+
+// // Will console the window
+// myFunction('ABC', '123');
+
+// // ".call" replaces the "this" context
+// // The first argument is the context, all other arguments are what the method expects
+// // Will console the myOjb
+// myFunction.call(myObj, 'DEF', '456');
+// // Will console an empty array
+// myFunction.call([], 'GHI', '789');
+
+// // Apply instead of call
+// // You pass only two arguments 
+// // 1) The context
+// // 2) An array of ALL other arguments
+// // "[c]all" is for [c]omma separated arguments
+// // "[a]pply" is for [a]rray arguments
+// myFunction.apply([], ['JKL', '10, 11, 12'])
+
+
+// // NOTE: call and apply have been around forever
+// // NOTE: bind is an ES5 feature
+
+
+// // Bind it now for calling later
+// // Again, the first paramter is the context and subsequent ones are the excpected function paramters
+// const boundFunction = myFunction.bind([], 'XYZ', '98, 99, 100');
+
+// // All of these have the same context and params as it's already been stored
+// boundFunction();
+// boundFunction();
+// boundFunction();
+
+// // The bound arguments cannot be overriden!!!
+// boundFunction(myObj, 'dude', 'wheres my car?');
+
+// // Now, we're ONLY binding the context
+// // We can pass parameters now but NOT change the context
+// const boundFunction2 = myFunction.bind([]);
+// boundFunction2('dude', 'wheres my car?');
+// boundFunction2('sup', 'dude');
+
+
+
+
+
+
+
+
+
+
+
 /**
- * Exploring "this" with .call, .apply, and .bind
+ * Arrow Functions and Lexical Scope
  */
 
-// Object Literal
-const myObj = {
-  stuff: '123',
-  myMethod() {
-    console.log('Object : ', this);
-  }
-}
+ // The value of "this" changes (in most cases) on how it is called
 
-// Will console the object
-// myObj.myMethod();
+ // eXample:
 
+ // myFunction(); // called from the window context so "this" === window
+ class MyClass {
+   myMethod() {
+     // New scope (lexical)
+     const foo = 123;
+     // the context of "this" is the class because
+     // it was called like `myInstance.myMethod`
 
-// Function
-function myFunction(text1: string, text2) {
-  console.log('Function : ', this, text1, text2);
-}
+     // To be able to reference the desired context later, you can save "this" to a variablle
+     const that = this;
+     console.log('1', this, foo);
+     setTimeout(function() {
+       // Another new scope (lexical)
+       // But "this" is the window object because it is called without a context
+       console.log('2', this, foo);
 
+       // Reference the lexical scope to get that context
+       console.log('3', that);
+       // 
+     }, 0);
 
-// Will console the window
-myFunction('ABC', '123');
+     // ES6 Array Functions solve "this"
+    setTimeout(() => {
+      // Arrow functions preserve the context from the parent!!
+      // "this" references the class instead of window
+      // An arrow function does NOT inherit "this" from the parent, it simply doesn't bind a new "this" 
+      // and therefore does not override the parent "this"
+      console.log('4', this);
+    }, 0);
+   }
+ }
 
-// ".call" replaces the "this" context
-// The first argument is the context, all other arguments are what the method expects
-// Will console the myOjb
-myFunction.call(myObj, 'DEF', '456');
-// Will console an empty array
-myFunction.call([], 'GHI', '789');
-
-// Apply instead of call
-// You pass only two arguments 
-// 1) The context
-// 2) An array of ALL other arguments
-// "[c]all" is for [c]omma separated arguments
-// "[a]pply" is for [a]rray arguments
-myFunction.apply([], ['JKL', '10, 11, 12'])
-
-
-// NOTE: call and apply have been around forever
-// NOTE: bind is an ES5 feature
-
-
-// Bind it now for calling later
-// Again, the first paramter is the context and subsequent ones are the excpected function paramters
-const boundFunction = myFunction.bind([], 'XYZ', '98, 99, 100');
-
-// All of these have the same context and params as it's already been stored
-boundFunction();
-boundFunction();
-boundFunction();
-
-// The bound arguments cannot be overriden!!!
-boundFunction(myObj, 'dude', 'wheres my car?');
-
-// Now, we're ONLY binding the context
-// We can pass parameters now but NOT change the context
-const boundFunction2 = myFunction.bind([]);
-boundFunction2('dude', 'wheres my car?');
-boundFunction2('sup', 'dude');
+ const myInstance = new MyClass();
+ myInstance.myMethod();
